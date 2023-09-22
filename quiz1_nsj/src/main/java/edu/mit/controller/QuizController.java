0b6295533ad6062fc3d7aa740edcf4c27f714140
NSJ <NSJ@DESKTOP-G9FKL7J>
2023-09-22 15:57:26 +0900

    8.get, post (quiz2) TwoVO 숫자를 전송하기

--- quiz1_nsj/src/main/java/edu/mit/controller/QuizController.java
+++ quiz1_nsj/src/main/java/edu/mit/controller/QuizController.java
@@ -3,13 +3,29 @@ package edu.mit.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import edu.mit.vo.TwoVO;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 @Controller
 public class QuizController {
 	
 	@GetMapping("/quiz/quiz1")
 	public void quiz(Model model) {
-		model.addAttribute("quiz", "오늘은 금요일");
-		
+		model.addAttribute("quiz", "오늘은 금요일");		
 	}
+	
+	@GetMapping("/quiz/quiz2")
+	public void quiz2() {
+		log.info("get요청");
+	}
+	
+	@PostMapping("/quiz/quiz2")
+	public void quiz2p(TwoVO vo) {
+		log.info("post 요청");
+		log.info(vo.getNum1()); log.info(vo.getNum2());
+	}
+	
 }
