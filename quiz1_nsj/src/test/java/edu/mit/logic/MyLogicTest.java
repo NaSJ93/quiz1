b3e35903a0cc1a24353d00b009bd699747fc6d0e
NSJ <NSJ@DESKTOP-G9FKL7J>
2023-09-22 14:30:09 +0900

    6. 오답 수정 static은 바로 사용 가능

--- quiz1_nsj/src/test/java/edu/mit/logic/MyLogicTest.java
+++ quiz1_nsj/src/test/java/edu/mit/logic/MyLogicTest.java
@@ -1,23 +1,19 @@
 package edu.mit.logic;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 public class MyLogicTest {
-	
-	@Autowired
-	MyLogic logic;
-	
+		
 	@Test
 	public void add() {
-		log.info("모두 더한 값 : "+logic.addup(1, 10));
+		log.info("모두 더한 값 : "+MyLogic.addup(1, 10));
 	}
 	
 	@Test
 	public void evenadd() {
-		log.info("짝수 더한 값 : "+logic.evenAddup(1, 10));
+		log.info("짝수 더한 값 : "+MyLogic.evenAddup(1, 10));
 	}
 }
